f1b863473bce6be0cbb5fac6a0470a0f1545b8e3
Fauzi455 <fauzirhmn45@gmail.com>
2024-10-04 10:32:25 +0700

    Add files via upload

---
+++ Pertanyaan4Percobaan1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class Pertanyaan4Percobaan1 {
+    public static void main(String[] args) {
+        Scanner input16 = new Scanner(System.in);
+    
+System.out.print("Masukkan tahun: ");
+int tahun = input16.nextInt();
+    
+if ((tahun % 100) == 0) {
+    if ((tahun % 400) == 0);
+    System.out.println("Tahun Kabisat");
+    } else 
+        System.out.println("Bukan Tahun Kabisat");
+            }
+        }
+
